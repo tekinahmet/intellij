bd8b2db929888063b99616fffc8f8db72528c4e9
tekinahmet <142019597+tekinahmet@users.noreply.github.com>
2023-08-14 16:16:16 +0300

    Update Class1.java

--- src/main/java/Class1.java
+++ src/main/java/Class1.java
@@ -1,5 +1,8 @@
 public class Class1 {
     public static void main(String[] args) {
-        System.out.println("version 1");
+    int a= 15;
+    int b= 25;
+    int c= a+b;
+        System.out.println(c);
     }
 }
