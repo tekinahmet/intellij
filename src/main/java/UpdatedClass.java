f5deea132dc44ad6157d9e2568b322a9a0442eaa
ahmet <aktekintr@gmail.com>
2023-08-15 15:03:59 +0300

    a+b

--- src/main/java/UpdatedClass.java
+++ src/main/java/UpdatedClass.java
@@ -1,5 +1,10 @@
 public class UpdatedClass {
   //string manipulation
+  public static void main(String[] args) {
+      int a = 5;
+      int b = 10;
+      System.out.println(a + b);
 
+  }
 
     }
